d1865ba4ae67948df7667d92a761a17abb9eccbf
Ian <ianskonto@googlemail.com>
2020-12-03 18:13:19 +0100

    Serialization Extensions

---
+++ CautiousDotNetExtensionLib/SerializerExtensions.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.Serialization.Formatters.Binary
+
+Module SerializerExtensions
+
+    <Extension()>
+    Public Sub SerializeToFile(BF As BinaryFormatter, obj As Object, Path As String)
+        Using stream As FileStream = File.Create(Path)
+            BF.Serialize(stream, obj)
+        End Using
+    End Sub
+
+    <Extension()>
+    Public Function DeserializeFromFile(BF As BinaryFormatter, Path As String) As Object
+        Dim v As Object = Nothing
+        Using Stream = File.OpenRead(Path)
+            v = BF.Deserialize(Stream)
+        End Using
+        Return v
+    End Function
+
+End Module
