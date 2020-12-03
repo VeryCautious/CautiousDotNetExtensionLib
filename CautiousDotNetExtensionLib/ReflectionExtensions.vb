7b989c80063e4b1be47583ae6006e2db66a88510
Ian <ianskonto@googlemail.com>
2020-12-03 18:13:30 +0100

    RelectionExtensions

---
+++ CautiousDotNetExtensionLib/ReflectionExtensions.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Reflection
+
+Module ReflectionExtensions
+
+    Public Function LoadInstances(Of T)(Optional args As Object() = Nothing) As IEnumerable(Of T)
+        Dim thisAsm As Assembly = Assembly.GetExecutingAssembly()
+        Dim types = thisAsm.GetTypes().Where(Function(ty As Type) (GetType(T).IsAssignableFrom(ty) AndAlso ty.IsClass AndAlso Not ty.IsAbstract)).ToList()
+
+        If args Is Nothing Then
+            Return types.Map(Function(typ As Type) CType(Activator.CreateInstance(typ), T))
+        Else
+            Return types.Map(Function(typ As Type) CType(Activator.CreateInstance(typ, args), T))
+        End If
+    End Function
+
+End Module
