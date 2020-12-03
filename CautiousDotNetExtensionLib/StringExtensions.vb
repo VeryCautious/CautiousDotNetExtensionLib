2cd73c3f43137bd39ce164e142a9ae0e127a2c05
Ian <ianskonto@googlemail.com>
2020-12-03 15:28:51 +0100

    Added simple String pattern matching

---
+++ CautiousDotNetExtensionLib/StringExtensions.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module StringExtensions
+
+    ''' <summary>
+    ''' Extracts data out of a String.
+    ''' </summary>
+    ''' <param name="S">The String that the data should be extraced from</param>
+    ''' <param name="Pattern">The pattern, where the capuring groups are labeled with {n} and n beeing the index in the result-array</param>
+    ''' <returns>An array of strings. At position i there is the value capured by group-i</returns>
+    <Extension()>
+    Public Function Match(ByVal S As String, ByVal Pattern As String) As String()
+        Dim ResultList As New Dictionary(Of Integer, String)
+
+        Dim IndexOfFirstMatch = Pattern.IndexOf("{"c)
+
+
+        While IndexOfFirstMatch >= 0
+            Dim sWork = S.Substring(0, IndexOfFirstMatch)
+            Dim pWork = Pattern.Substring(0, IndexOfFirstMatch)
+
+            If Not pWork = sWork Then
+                Throw New ArgumentException("Does not match pattern")
+            End If
+
+            S = S.Substring(IndexOfFirstMatch)
+            Pattern = Pattern.Substring(IndexOfFirstMatch)
+
+            If Pattern.IndexOf("}"c) = Pattern.Length - 1 Then
+                ResultList.Add(CInt(GetValueBetweenTwoIndex(Pattern, Pattern.IndexOf("{"c), Pattern.IndexOf("}"c))), S)
+                Pattern = ""
+            Else
+                Dim SMatchEnd = S.IndexOf(Pattern(Pattern.IndexOf("}"c) + 1))
+                Dim SMatch = S.Substring(0, SMatchEnd)
+                ResultList.Add(CInt(GetValueBetweenTwoIndex(Pattern, Pattern.IndexOf("{"c), Pattern.IndexOf("}"c))), SMatch)
+
+                Pattern = Pattern.Substring(Pattern.IndexOf("}"c) + 1)
+                S = S.Substring(SMatchEnd)
+            End If
+
+            IndexOfFirstMatch = Pattern.IndexOf("{"c)
+        End While
+
+
+
+        Dim IndexCount = ResultList.Keys.Max
+        Dim resArray(IndexCount) As String
+
+        For Each Item In ResultList
+            resArray(Item.Key) = Item.Value
+        Next
+
+        Return resArray
+    End Function
+
+    Private Function GetValueBetweenTwoIndex(S As String, index1 As Integer, index2 As Integer) As String
+        Return S.Substring(index1 + 1, (index2 - index1) - 1)
+    End Function
+
+
+End Module
---
+++ XUnitTestProject/StringExtensionTest.vb
@@ -0,0 +1,41 @@
+﻿Imports CautiousDotNetExtensionLib.StringExtensions
+Imports Xunit
+Namespace XUnitTestProject
+    Public Class StringExtensionTest
+
+        <Fact>
+        Sub TestPatternMatch1()
+            Dim s As String = "1:2:3"
+            Dim Matches = s.Match("{0}:{1}:{2}")
+            Assert.Equal("1", Matches(0))
+            Assert.Equal("2", Matches(1))
+            Assert.Equal("3", Matches(2))
+        End Sub
+
+        <Fact>
+        Sub TestPatternMatch2()
+            Dim s As String = "(1.23,4.56,-\78.9)"
+            Dim Matches = s.Match("({0},{1},-\{2}){3}")
+            Assert.Equal("1.23", Matches(0))
+            Assert.Equal("4.56", Matches(1))
+            Assert.Equal("78.9", Matches(2))
+            Assert.Equal("", Matches(3))
+        End Sub
+
+        <Fact>
+        Sub TestPatternMatch3()
+            Dim s As String = "Hello Elisa Merkel."
+            Dim Matches = s.Match("Hello {1} {0}.")
+            Assert.Equal("Merkel", Matches(0))
+            Assert.Equal("Elisa", Matches(1))
+        End Sub
+
+        <Fact>
+        Sub TestPatternMatch4()
+            Dim s As String = "odiwahjdop"
+            Dim Matches = s.Match("{5}")
+            Assert.Equal(s, Matches(5))
+        End Sub
+
+    End Class
+End Namespace
