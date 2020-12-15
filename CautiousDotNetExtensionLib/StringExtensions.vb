7890be435742cc2e8347d68171a8ff8c35b99ed5
Ian <ianskonto@googlemail.com>
2020-12-15 13:02:32 +0100

    Enhanced the String match function

--- CautiousDotNetExtensionLib/StringExtensions.vb
+++ CautiousDotNetExtensionLib/StringExtensions.vb
@@ -2,77 +2,101 @@
 
 Public Module StringExtensions
 
-    ''' <summary>
-    ''' Extracts data out of a String. Throws an exeption if the string does not match
-    ''' !! Within the capuring group there can not be a char that is after the group or the match will fail!!
-    ''' for example when you have the pattern "{0}n" then the String to match can not have a n before the last char.
-    ''' </summary>
-    ''' <param name="S">The String that the data should be extraced from</param>
-    ''' <param name="Pattern">The pattern, where the capuring groups are labeled with {n} and n beeing the index in the result-array</param>
-    ''' <returns>An array of strings. At position i there is the value capured by group-i</returns>
+
     <Extension()>
-    Public Function Match(ByVal S As String, ByVal Pattern As String) As String()
-        Dim ret = TryMatch(S, Pattern)
+    Public Function Match(ByVal S As String, ByVal Pattern As String, Optional Greedy As Boolean = False) As String()
+        Dim ret = TryMatch(S, Pattern, Greedy)
         If ret Is Nothing Then
             Throw New ArgumentException("Does not match pattern")
         End If
         Return ret
     End Function
 
-    ''' <summary>
-    ''' Extracts data out of a String. Returns Nothing if the String does not match
-    ''' !! Within the capuring group there can not be a char that is after the group or the match will fail!!
-    ''' for example when you have the pattern "{0}n" then the String to match can not have a n before the last char.
-    ''' </summary>
-    ''' <param name="S">The String that the data should be extraced from</param>
-    ''' <param name="Pattern">The pattern, where the capuring groups are labeled with {n} and n beeing the index in the result-array</param>
-    ''' <returns>An array of strings. At position i there is the value capured by group-i</returns>
     <Extension()>
-    Public Function TryMatch(ByVal S As String, ByVal Pattern As String) As String()
-        Dim ResultList As New Dictionary(Of Integer, String)
+    Public Function TryMatch(ByVal S As String, ByVal Pattern As String, Optional Greedy As Boolean = False) As String()
+        Dim ret = S.MatchToDict(Pattern, Greedy)
 
-        Dim IndexOfFirstMatch = Pattern.IndexOf("{"c)
+        If Not ret.FoundMatch Then
+            Return Nothing
+        End If
 
-
-        While IndexOfFirstMatch >= 0
-            Dim sWork = S.Substring(0, IndexOfFirstMatch)
-            Dim pWork = Pattern.Substring(0, IndexOfFirstMatch)
-
-            If Not pWork = sWork Then
-                Return Nothing
-            End If
-
-            S = S.Substring(IndexOfFirstMatch)
-            Pattern = Pattern.Substring(IndexOfFirstMatch)
-
-            If Pattern.IndexOf("}"c) = Pattern.Length - 1 Then
-                ResultList.Add(CInt(GetValueBetweenTwoIndex(Pattern, Pattern.IndexOf("{"c), Pattern.IndexOf("}"c))), S)
-                Pattern = ""
-            Else
-                Dim SMatchEnd = S.IndexOf(Pattern(Pattern.IndexOf("}"c) + 1))
-                Dim SMatch = S.Substring(0, SMatchEnd)
-                ResultList.Add(CInt(GetValueBetweenTwoIndex(Pattern, Pattern.IndexOf("{"c), Pattern.IndexOf("}"c))), SMatch)
-
-                Pattern = Pattern.Substring(Pattern.IndexOf("}"c) + 1)
-                S = S.Substring(SMatchEnd)
-            End If
-
-            IndexOfFirstMatch = Pattern.IndexOf("{"c)
-        End While
-
-
-
-        Dim IndexCount = ResultList.Keys.Max
+        Dim IndexCount = ret.Matches.Keys.Max
         Dim resArray(IndexCount) As String
 
-        For Each Item In ResultList
+        For Each Item In ret.Matches
             resArray(Item.Key) = Item.Value
         Next
 
         Return resArray
     End Function
 
-    Private Function GetValueBetweenTwoIndex(S As String, index1 As Integer, index2 As Integer) As String
+    ''' <summary>
+    ''' Basicly reverses the String.Format() function by splitting a string into parts.
+    ''' The pattern defines the structure of the string with some capturing groups that the function tries to match.
+    ''' For instance the String "(a,b,c)" with the pattern "({1},{0})" will result in a dictionary with two values:
+    ''' Dict(0) = "b,c" and Dict(1) = "a". The default is not greedy!
+    ''' </summary>
+    ''' <param name="Greedy">If set to true the Capturing group caputers as mutch as it can whilst still matching</param>
+    ''' <returns></returns>
+    <Extension()>
+    Public Function MatchToDict(ToMatch As String, Pattern As String, Optional Greedy As Boolean = False) As (Matches As Dictionary(Of Integer, String), FoundMatch As Boolean)
+        Dim FirstPattern As Integer = Pattern.IndexOf("{")
+
+        If FirstPattern < 0 Then
+            If ToMatch = Pattern Then
+                Return (New Dictionary(Of Integer, String), True)
+            Else
+                Return (Nothing, False)
+            End If
+        End If
+
+        If Not ToMatch.Substring(0, FirstPattern) = Pattern.Substring(0, FirstPattern) Then
+            Return (Nothing, False)
+        End If
+
+        Dim MatchIndex = CInt(Pattern.Substring(FirstPattern + 1, Pattern.IndexOf("}"c) - (FirstPattern + 1)))
+
+        If ToMatch.Count <= FirstPattern Then
+            Return (New Dictionary(Of Integer, String) From {{MatchIndex, ""}}, True)
+        End If
+
+        ToMatch = ToMatch.Substring(FirstPattern)
+        Pattern = Pattern.Substring(Pattern.IndexOf("}") + 1)
+
+        If Pattern = "" Then
+            Return (New Dictionary(Of Integer, String) From {{MatchIndex, ToMatch}}, True)
+        End If
+
+        Dim PossibleMatches As New List(Of (Index As Integer, Matched As String))
+
+        Dim StartVal As Integer = 0
+        Dim EndVal As Integer = ToMatch.Count - 1
+        Dim StepSize As Integer = 1
+
+        If Greedy Then
+            StartVal = ToMatch.Count - 1
+            EndVal = 0
+            StepSize = -1
+        End If
+
+        For I As Integer = StartVal To EndVal Step StepSize
+            If ToMatch(I) = Pattern(0) Then PossibleMatches.Add((I, ToMatch.Substring(0, I)))
+        Next
+
+
+        For Each M In PossibleMatches
+            Dim res = MatchToDict(ToMatch.Substring(M.Index), Pattern)
+            If res.FoundMatch Then
+                res.Matches.Add(MatchIndex, M.Matched)
+                Return res
+            End If
+        Next
+
+        Return (Nothing, False)
+    End Function
+
+    <Extension()>
+    Public Function GetValueBetweenTwoIndex(S As String, index1 As Integer, index2 As Integer) As String
         Return S.Substring(index1 + 1, (index2 - index1) - 1)
     End Function
 
